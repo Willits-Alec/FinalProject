26c390048f5007c9e0631a2b3ca008c476577f2a
mtyra321 <mtyra321@gmail.com>
2020-11-18 18:35:24 -0700

    collisions now happen

--- game3/cars.js
+++ game3/cars.js
@@ -15,13 +15,21 @@
 
 //var cars;
 class Car {
-    constructor(image, x, y, isLeft, speed) {
+    constructor(image, x, y, isLeft, speed, width, height) {
         this.image = image;
         this.x = x
         this.y = y;
         this.isLeft = isLeft;
         this.speed = speed;
+        this.width = width;
+        this.height = height;
 
+    }
+    intersects(character) {
+        return !(character.x > (this.x + this.width) ||
+            (character.x + character.width) < this.x ||
+            character.y > (this.y + this.height) ||
+            (character.y + character.height) < this.y);
     }
 }
 
--- game3/frogger.js
+++ game3/frogger.js
@@ -10,19 +10,20 @@ add hitbox
 */
 import Car from "./cars.js";
 import Character from "./character.js";
-const carList = [];
+var carList = [];
 
 var canvas, ctx;
 var character;
 var score;
 
 function setUpCanvas() {
+    carList = [];
     score = 0;
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     let characterImage = new Image();
     characterImage.src = "Pictures/frogger/frogger_forward.png";
-    character = new Character(characterImage, (canvas.width / 2) - 50, (canvas.height - 50), 5);
+    character = new Character(characterImage, (canvas.width / 2) - 50, (canvas.height - 50), 5, 50, 50);
     populateCars();
     animate();
     document.getElementsByTagName("body")[0].addEventListener("keydown", function(e) {
@@ -59,12 +60,10 @@ function setUpCanvas() {
 
 
 
-window.onload = setUpCanvas;
+document.getElementById("start").addEventListener('click', setUpCanvas);
 
 function updateScore(c) {
     if (character.y < 750 && character.y > 0) {
-        console.log("in if");
-        console.log(character.y);
         score += c;
     }
 }
@@ -73,11 +72,14 @@ function animate() {
     // console.log(character.y);
 
     document.getElementById("score").innerText = "Score: " + score;
+    document.getElementById("lives").innerText = "Lives: " + character.lives;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-    ctx.drawImage(character.image, character.x, character.y, 50, 50);
+    ctx.drawImage(character.image, character.x, character.y, character.width, character.height);
+    checkForCollisions();
     for (let i = 0; i < carList.length; i++) {
         let element = carList[i];
-        ctx.drawImage(element.image, element.x, element.y, 50, 50);
+        ctx.drawImage(element.image, element.x, element.y, element.width, element.height);
 
 
         element.x += element.speed;
@@ -85,6 +87,37 @@ function animate() {
             element.x = -100;
         }
     }
+    boundaries();
+    if (character.lives == 0) {
+        endGame();
+
+
+        return;
+    }
+    requestAnimationFrame(animate) // loop
+
+
+}
+
+function endGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+    document.getElementById("lives").innerText = "Lives: " + character.lives;
+}
+
+function checkForCollisions() {
+    for (let index = 0; index < carList.length; index++) {
+        let element = carList[index];
+        if (element.intersects(character)) {
+            character.collision();
+        }
+
+
+    }
+}
+
+
+
+function boundaries() {
     if (character.x >= 800) {
         character.x = 750;
     }
@@ -97,20 +130,17 @@ function animate() {
     if (character.x <= 0) {
         character.x = 0;
     }
-
-    requestAnimationFrame(animate) // loop
-
 }
 
 function populateCars() {
     let image = new Image();
     image.src = "Pictures/cars/car2.png";
-    let car = new Car(image, 0, 0, false, 5);
+    let car = new Car(image, 0, 0, false, 5, 50, 50);
     carList.push(car);
-    car = new Car(image, 0, 90, false, 3);
+    car = new Car(image, 0, 90, false, 3, 200, 50);
     carList.push(car);
-    car = new Car(image, 0, 700, false, 1);
+    car = new Car(image, 0, 700, false, 1, 300, 300);
     carList.push(car);
-    car = new Car(image, 0, 240, false, 12);
+    car = new Car(image, 0, 240, false, 12, 25, 25);
     carList.push(car);
 }
